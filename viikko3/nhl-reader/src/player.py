028747a0bd9ba6e73a88512953ca4e30e66b54d2
Jukka Aho <ahojukka5@gmail.com>
2021-01-28 23:04:10 +0700

    Add more attributes to Player

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, assists, goals, penalties, team, games):
         self.name = name
-    
+        self.nationality = nationality
+        self.assists = assists
+        self.goals = goals
+        self.penalties = penalties
+        self.team = team
+        self.games = games
+
     def __str__(self):
-        return self.name
+        return f"{self.name} team {self.team} goals {self.goals} assists {self.assists}"
