cc63aa121120515ea06b29d485afcaf13b450db6
Jukka Aho <ahojukka5@gmail.com>
2021-01-28 23:23:36 +0700

    Use PlayerReader and PlayerStats

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,39 +1,19 @@
 from datetime import datetime
-import requests
 
-from player import Player
+from player_reader import PlayerReader
+from player_stats import PlayerStats
 
 
 def main(verbose=False):
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
-    response = requests.get(url).json()
-
-    if verbose:
-        print("JSON-muotoinen vastaus:")
-        print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(
-            player_dict["name"],
-            player_dict["nationality"],
-            player_dict["assists"],
-            player_dict["goals"],
-            player_dict["penalties"],
-            player_dict["team"],
-            player_dict["games"],
-        )
-
-        players.append(player)
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
 
     nationality = "FIN"
     now = datetime.now()
     print(f"Players from {nationality} {now}")
-
-    players_ = list(filter(lambda p: p.nationality == nationality, players))
-    players_.sort(key=lambda p: p.points, reverse=True)
-    for player in players_:
+    players = stats.top_scorers_by_nationality(nationality)
+    for player in players:
         print(player)
 
 
