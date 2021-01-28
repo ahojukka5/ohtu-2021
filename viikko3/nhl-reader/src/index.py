d1224206305cd0541e723b4252063a1adada9eff
Jukka Aho <ahojukka5@gmail.com>
2021-01-28 23:04:28 +0700

    List all players from Finland

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,24 +1,37 @@
+from datetime import datetime
 import requests
+
 from player import Player
 
 
-def main():
+def main(verbose=False):
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
+    if verbose:
+        print("JSON-muotoinen vastaus:")
+        print(response)
 
     players = []
 
     for player_dict in response:
-        player = Player(player_dict["name"])
+        player = Player(
+            player_dict["name"],
+            player_dict["nationality"],
+            player_dict["assists"],
+            player_dict["goals"],
+            player_dict["penalties"],
+            player_dict["team"],
+            player_dict["games"],
+        )
 
         players.append(player)
 
-    print("Oliot:")
+    nationality = "FIN"
+    now = datetime.now()
+    print(f"Players from {nationality} {now}")
 
-    for player in players:
+    for player in filter(lambda p: p.nationality == nationality, players):
         print(player)
 
 
