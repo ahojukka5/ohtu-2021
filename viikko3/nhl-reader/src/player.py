86a7af48ea94e2fff24264549c16ed2eee33b565
Jukka Aho <ahojukka5@gmail.com>
2021-01-28 23:13:00 +0700

    Make __str__ look prettier

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -13,4 +13,4 @@ class Player:
         return self.goals + self.assists
 
     def __str__(self):
-        return f"{self.name} team {self.team} goals {self.goals} assists {self.assists}"
+        return f"{self.name:20} {self.team:3} {self.goals:2} + {self.assists:2} = {self.points:2}"
