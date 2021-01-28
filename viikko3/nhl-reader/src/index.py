a2dd16f721cf8e59a560e76b21bca05a7eef5763
Jukka Aho <ahojukka5@gmail.com>
2021-01-28 22:48:25 +0700

    Initial template for script

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,26 @@
+import requests
 from player import Player
 
+
 def main():
-    pass
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict["name"])
+
+        players.append(player)
+
+    print("Oliot:")
+
+    for player in players:
+        print(player)
+
 
 if __name__ == "__main__":
     main()
