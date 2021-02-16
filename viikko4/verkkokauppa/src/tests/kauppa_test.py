f779d6b70b7e445761747ef108e82b4e52f3c032
Jukka Aho <ahojukka5@gmail.com>
2021-02-16 22:03:23 +0700

    Add two different products

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -81,3 +81,51 @@ class TestKauppa(unittest.TestCase):
         tilille = ANY
         summa = 5
         pankki_mock.tilisiirto.assert_called_with(nimi, viitenumero, tililta, tilille, summa)
+
+    def test_kaupppareissu_2(self):
+        """Aloitetaan asiointi, koriin lisätään kaksi eri tuotetta, joita
+        varastossa on ja suoritetaan ostos, varmista että kutsutaan pankin
+        metodia tilisiirto oikealla asiakkaalla, tilinumerolla ja summalla."""
+
+        pankki_mock = Mock()
+        viitegeneraattori_mock = Mock()
+
+        # palautetaan aina arvo 42
+        viitegeneraattori_mock.uusi.return_value = 42
+
+        varasto_mock = Mock()
+
+        # tehdään toteutus saldo-metodille
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+            if tuote_id == 2:
+                return 10
+
+        # tehdään toteutus hae_tuote-metodille
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "maito", 5)
+            if tuote_id == 2:
+                return Tuote(2, "jauheliha", 10)
+
+        # otetaan toteutukset käyttöön
+        varasto_mock.saldo.side_effect = varasto_saldo
+        varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        # alustetaan kauppa
+        kauppa = Kauppa(varasto_mock, pankki_mock, viitegeneraattori_mock)
+
+        # tehdään ostokset
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.lisaa_koriin(2)
+        kauppa.tilimaksu("pekka", "12345")
+
+        # varmistetaan, että metodia tilisiirto on kutsuttu
+        nimi = "pekka"
+        viitenumero = 42
+        tililta = "12345"
+        tilille = ANY
+        summa = 15
+        pankki_mock.tilisiirto.assert_called_with(nimi, viitenumero, tililta, tilille, summa)
