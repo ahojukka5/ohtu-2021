07da1653aebc2354de330edf1a563237fad5018c
Jukka Aho <ahojukka5@gmail.com>
2021-02-16 21:43:09 +0700

    Load only positive amount of money

--- viikko4/maksukortti-mock/src/kassapaate.py
+++ viikko4/maksukortti-mock/src/kassapaate.py
@@ -6,7 +6,8 @@ class Kassapaate:
         self.myytyja_lounaita = 0
 
     def lataa(self, kortti, summa):
-        kortti.lataa(summa)
+        if summa > 0:
+            kortti.lataa(summa)
 
     def osta_lounas(self, kortti):
         if kortti.saldo > HINTA:
