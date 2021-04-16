1d2be2d89c3890222d75e594942bdcc3c536a1b4
Jukka Aho <ahojukka5@gmail.com>
2021-04-16 22:28:53 +0700

    Add some test code to try queries

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from querybuilder import QueryBuilder
 
 
 def main():
@@ -8,11 +8,12 @@ def main():
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(5, "assists"),
-        PlaysIn("PHI")
-    )
+    query = QueryBuilder()
+    # matcher = query.playsIn("NYR").build()
+    # matcher = query.playsIn("NYR").hasAtLeast(5, "goals").hasFewerThan(10, "goals").build()
+    m1 = query.playsIn("PHI").hasAtLeast(10, "assists").hasFewerThan(5, "goals").build()
+    m2 = query.playsIn("EDM").hasAtLeast(40, "points").build()
+    matcher = query.oneOf(m1, m2).build()
 
     for player in stats.matches(matcher):
         print(player)
