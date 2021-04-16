55a2b75ccb897fdfcbba7428cdc17f431be78be0
Jukka Aho <ahojukka5@gmail.com>
2021-04-16 15:43:51 +0700

    Refactor to use command design pattern

--- viikko5/laskin/src/kayttoliittyma.py
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -9,14 +9,58 @@ class Komento(Enum):
     KUMOA = 4
 
 
+class PerusKomento:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def lue_syote(self):
+        arvo = 0
+        try:
+            arvo = int(self._lue_syote())
+        except Exception:
+            pass
+        return arvo
+
+    def suorita(self):
+        raise NotImplementedError("Implementoi itse!")
+
+
+class Summa(PerusKomento):
+    def suorita(self):
+        self._sovelluslogiikka.plus(self.lue_syote())
+
+
+class Erotus(PerusKomento):
+    def suorita(self):
+        self._sovelluslogiikka.miinus(self.lue_syote())
+
+
+class Nollaus(PerusKomento):
+    def suorita(self):
+        self._sovelluslogiikka.nollaa()
+
+
+class Kumoa(PerusKomento):
+    def suorita(self):
+        pass
+
+
 class Kayttoliittyma:
-    def __init__(self, sovellus, root):
-        self._sovellus = sovellus
+    def __init__(self, sovelluslogiikka, root):
+        self._sovelluslogiikka = sovelluslogiikka
         self._root = root
+
+        self._komennot = {
+            Komento.SUMMA: Summa(sovelluslogiikka, self._lue_syote),
+            Komento.EROTUS: Erotus(sovelluslogiikka, self._lue_syote),
+            Komento.NOLLAUS: Nollaus(sovelluslogiikka, self._lue_syote),
+            Komento.KUMOA: Kumoa(sovelluslogiikka, self._lue_syote),
+        }
 
     def kaynnista(self):
         self._tulos_var = StringVar()
-        self._tulos_var.set(self._sovellus.tulos)
+        self._tulos_var.set(self._sovelluslogiikka.tulos)
         self._syote_kentta = ttk.Entry(master=self._root)
 
         tulos_teksti = ttk.Label(textvariable=self._tulos_var)
@@ -24,27 +68,27 @@ class Kayttoliittyma:
         summa_painike = ttk.Button(
             master=self._root,
             text="Summa",
-            command=lambda: self._suorita_komento(Komento.SUMMA)
+            command=lambda: self._suorita_komento(Komento.SUMMA),
         )
 
         erotus_painike = ttk.Button(
             master=self._root,
             text="Erotus",
-            command=lambda: self._suorita_komento(Komento.EROTUS)
+            command=lambda: self._suorita_komento(Komento.EROTUS),
         )
 
         self._nollaus_painike = ttk.Button(
             master=self._root,
             text="Nollaus",
             state=constants.DISABLED,
-            command=lambda: self._suorita_komento(Komento.NOLLAUS)
+            command=lambda: self._suorita_komento(Komento.NOLLAUS),
         )
 
         self._kumoa_painike = ttk.Button(
             master=self._root,
             text="Kumoa",
             state=constants.DISABLED,
-            command=lambda: self._suorita_komento(Komento.KUMOA)
+            command=lambda: self._suorita_komento(Komento.KUMOA),
         )
 
         tulos_teksti.grid(columnspan=4)
@@ -54,29 +98,18 @@ class Kayttoliittyma:
         self._nollaus_painike.grid(row=2, column=2)
         self._kumoa_painike.grid(row=2, column=3)
 
+    def _lue_syote(self):
+        return self._syote_kentta.get()
+
     def _suorita_komento(self, komento):
-        arvo = 0
-
-        try:
-            arvo = int(self._syote_kentta.get())
-        except Exception:
-            pass
-
-        if komento == Komento.SUMMA:
-            self._sovellus.plus(arvo)
-        elif komento == Komento.EROTUS:
-            self._sovellus.miinus(arvo)
-        elif komento == Komento.NOLLAUS:
-            self._sovellus.nollaa()
-        elif komento == Komento.KUMOA:
-            pass
-
+        komento_olio = self._komennot[komento]
+        komento_olio.suorita()
         self._kumoa_painike["state"] = constants.NORMAL
 
-        if self._sovellus.tulos == 0:
+        if self._sovelluslogiikka.tulos == 0:
             self._nollaus_painike["state"] = constants.DISABLED
         else:
             self._nollaus_painike["state"] = constants.NORMAL
 
         self._syote_kentta.delete(0, constants.END)
-        self._tulos_var.set(self._sovellus.tulos)
+        self._tulos_var.set(self._sovelluslogiikka.tulos)
