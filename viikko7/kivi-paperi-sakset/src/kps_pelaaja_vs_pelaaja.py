42e1732d239d76bdcb088091d64762a715f9bce7
Jukka Aho <ahojukka5@gmail.com>
2021-04-20 19:47:31 +0700

    Refactor kps_pelaaja_vs_pelaaja.py

--- viikko7/kivi-paperi-sakset/src/kps_pelaaja_vs_pelaaja.py
+++ viikko7/kivi-paperi-sakset/src/kps_pelaaja_vs_pelaaja.py
@@ -1,22 +1,6 @@
-from tuomari import Tuomari
+from kps import KPS
 
 
-class KPSPelaajaVsPelaaja:
-    def pelaa(self):
-        tuomari = Tuomari()
-
-        ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-        tokan_siirto = input("Toisen pelaajan siirto: ")
-
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
-            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            print(tuomari)
-
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            tokan_siirto = input("Toisen pelaajan siirto: ")
-
-        print("Kiitos!")
-        print(tuomari)
-
-    def _onko_ok_siirto(self, siirto):
-        return siirto == "k" or siirto == "p" or siirto == "s"
+class KPSPelaajaVsPelaaja(KPS):
+    def _toisen_siirto(self, siirto):
+        return input("Toisen pelaajan siirto: ")
